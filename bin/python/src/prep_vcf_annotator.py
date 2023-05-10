aad28388410792b362a378a20cffe0fe095b9a73
evezeyl <evezeyl@gmail.com>
2023-05-10 16:50:29 +0200

    added prep_vcf_annotator should be ok

--- bin/python/src/prep_vcf_annotator.py
+++ bin/python/src/prep_vcf_annotator.py
@@ -1,79 +1,54 @@
 import argparse
 import os
-import sys
+import pandas as pd
 
 parser = argparse.ArgumentParser(
     prog="prep_vcf_annotator.py",
     description='Prepare annotation with vcf annotator: correct format vcf from nucdiff output. Prepares script launch command for vcf annotator',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("--vcf1",
+parser.add_argument("--vcf",
                     action="store",
                     required=True,
-                    help="vcf file output 1 from nucdiff")
-parser.add_argument("--vcf2",
-                    action="store",
-                    required=True,
-                    help="vcf file output 2 from nucdiff")
-parser.add_argument("--gbff1",
-                    action="store",
-                    required=True,
-                    help="gbff file output 1 from bakta annotation")
-parser.add_argument("--gbff2",
-                    action="store",
-                    required=True,
-                    help="gbff file output 2 from bakta annotation")
+                    help="vcf file from nucdiff")
 parser.add_argument("--outdir",
                     action="store",
                     default=".",
-                    help="path of output directory for vcf annotator")
-parser.add_argument("--outdir_script",
-                    action="store",
-                    default=".",
-                    help="path where the script to run vcf annotator will be deposited")
-parser.add_argument("--test",
-                    action="store",
-                    default=False,
-                    help="Source paths for testing. Paths defined in test_paths.py")
-
-
+                    help="oudir for reformated file")
 args = vars(parser.parse_args())
 
-# sourcing functions dir
-sys.path.append(os.path.dirname(__file__) + "/" + "diff")
+# %% functions
+def reformat_vcf(file, outdir = ".", skip_rows = 2):
+    """ reformat the vcf from nucdiff so it can be used in downstream analyses. Adds the "INFO column that was missing."
+    :param file: vcf file from nucdiff
+    :param skip_rows: default 2 (normal formatting from nucdiff vcf file)
+    :param outdir: where the reformated vcf will be deposited
+    :return paths of reformated files
+    """
+    # get the header
+    with open(file) as input:
+        vcf_head = [next(input) for _ in range(skip_rows)]
+    input.close()
 
-# default test directories here
-if args["test"]:
-    sys.path.append(os.path.dirname(__file__) + "/" + "test")
-    import test_paths
-    pc_out = os.getenv("OUTPUTDIR")
-    #print("pc_out: " + pc_out)
-    args["outdir"] = pc_out
-    args["outdir_script"] = pc_out
+    vcf_head = "".join(i for i in vcf_head)
 
-# get the paths if ".":
-## cannot work for test
-if not args["test"] and args["outdir"] == ".":
-    args["outdir"] = os.getcwd()
+    # get the body
+    df = pd.read_table(file, sep="\t", skiprows= skip_rows, skip_blank_lines=True, index_col=None)
+    df = df.assign(INFO = ".")
 
-if not args["test"] and args["outdir_script"] == ".":
-    args["outdir_script"] = os.getcwd()
+    # write new vcf file
+    new_file = file.replace(".vcf", "_reformated.vcf")
 
-# script
-# we have two vcf files to prepare (ref and query referential
-from reformat_vcf import reformat_vcf
-reformated_vcf1 = reformat_vcf(args["vcf1"], args["outdir"])
-reformated_vcf2 = reformat_vcf(args["vcf2"], args["outdir"])
+    ## path output
+    if (outdir == "."):
+        new_file = os.path.basename(new_file)
+    else:
+        new_file = outdir + "/" + os.path.basename(new_file)
 
-# detecting which is according to coordinate ref or query
-if "_ref_snps.vcf" in args["vcf1"]:
-    ref_vcf = reformated_vcf1
-    query_vcf = reformated_vcf2
-else:
-    ref_vcf = reformated_vcf2
-    query_vcf = reformated_vcf1
+    with open(new_file, 'w') as new:
+        new.write(vcf_head)
+    new.close()
+    df.to_csv(new_file, sep="\t", mode="a", index=False)
 
-# detection of which one should be used as ref or query as coordinate system for annotation is in function
-from create_run_vcf_annotator import create_run_vcf_annotator
-create_run_vcf_annotator(ref_vcf, query_vcf, args["gbff1"] , args["gbff2"], args["outdir"], args["outdir_script"])
+reformat_vcf(args["vcf"], args["outdir"])
 
