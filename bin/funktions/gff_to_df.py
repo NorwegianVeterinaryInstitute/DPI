0c984d1bcdb335545bc413d285a85868017d2ecc
Eve Zeyl Fiskebeck <evezeyl@gmail.com>
2025-04-10 13:44:40 +0200

    gff_to_df improved and working

--- bin/funktions/gff_to_df.py
+++ bin/funktions/gff_to_df.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python
+
+# SECTION : Imports
+import argparse
 import os
 import gffpandas.gffpandas as gffpd
-import pandas as pd
-import numpy as np
-import re
+import datetime
+import sys
+import logging
 
+# !SECTION
+
+# SECTION : Functions definitions
 def gff_to_df(file_path):
     """
     Reads a GFF file and converts it into a pandas DataFrame.
@@ -52,3 +59,76 @@ def gff_to_df(file_path):
     except Exception as e:
         print(f"Error processing GFF data for {file_path}: {e}")
         return None
+
+# SECTION MAIN
+if __name__ == "__main__":
+    # SECTION : Argument parsing
+    parser = argparse.ArgumentParser(
+        prog="gff_to_df.py",
+        description="Creates a table csv file from a gff file.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        add_help=True,)
+    
+    # Version and example arguments (optional)
+    parser.add_argument(
+        "--example",
+        action="store_true",
+        help="Show an example of usage and exit.",
+        )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s 0.0.2",
+        help="Print the script version and exit.",
+        )
+    # Required arguments
+    parser.add_argument("--file_path", required=True, help="Path to the result file",)
+    
+    args = parser.parse_args()
+    # !SECTION
+    
+    # SECTION : Check if required arguments are provided
+    if not all([args.file_path]):
+        parser.error(
+            "The following arguments are required: --file_path"
+        )
+        sys.exit(1)      
+    # !SECTION
+    
+    # SECTION : Handling of example
+    if args.example:
+        logging.info("Example usage:")
+        logging.info("python gff_to_df.py --file_path <path_to_file>")
+    # !SECTION
+    
+
+    # SECTION : Login info output
+    log_file_name = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_gff_to_df.log"
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_file_name, mode="w"),
+            logging.StreamHandler(sys.stdout),
+        ],
+        )
+    # !SECTION
+    
+    # SECTION : SCRIPT : create the data frame and export as csv files
+    try:
+        logging.info(f"processing result file ${args.file_path}")
+        current_working_directory = os.getcwd()
+        df = gff_to_df(args.file_path)
+        base_name, extension = os.path.splitext(os.path.basename(args.file_path))        
+
+        df.to_csv(
+            os.path.join(current_working_directory, f"{base_name}.csv"),
+            index=False,
+            header=True
+            )
+    except Exception as e:
+        logging.error(f"An error occurred during the processing of result files: {e}")
+        logging.error(f"Check {log_file_name} for more details")
+    # !SECTION
+# !SECTION
