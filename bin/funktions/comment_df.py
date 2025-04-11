69b0401d34b3dd025ecb6e9b0ad41cffe77819b3
Eve Zeyl Fiskebeck <evezeyl@gmail.com>
2025-04-11 14:04:28 +0200

    update defensive

--- bin/funktions/comment_df.py
+++ bin/funktions/comment_df.py
@@ -1,6 +1,12 @@
 # Creates a comment dataframe
 # NOTE : I do not think I make this one a script - as wont be used outside of the pipeline
+import os
+import sys
+
 import pandas as pd
+from error_template import log_message
+from error_template import processing_error_message
+from error_template import processing_result_message
 
 
 def create_comment_df (identifier, comment): 
@@ -12,22 +18,32 @@ def create_comment_df (identifier, comment):
     Returns:
         DataFrame: simple pandas dataframe
     """
-    
+    script_name = os.path.basename(__file__)
+    info_message = processing_result_message(script_name, "NA - comment table creation")
+    print(info_message)
+    log_message(info_message, script_name)
+
     # identifier can be ref or ref_query 
-    
     try: 
         parts = identifier.split("_")
         ref, query = parts[0], parts[1]
+        
     # if there is query is empty or does not exist
     except IndexError:
         ref = identifier
         query = None
+        
+        
     # Create a DataFrame with the comment
-    data = {
-        "ref": [ref],
-        "query": [query],
-        "comment": [comment]
-    }
-    df = pd.DataFrame(data)
-    return df
-    
+    try: 
+        data = {
+            "ref": [ref],
+            "query": [query],
+            "comment": [comment]
+        }
+        df = pd.DataFrame(data)
+        return df
+    except Exception as e:
+        error_message = f"Error creating comment table: {e}"
+        print(error_message)
+        log_message(error_message, script_name, exit_code=1) 
