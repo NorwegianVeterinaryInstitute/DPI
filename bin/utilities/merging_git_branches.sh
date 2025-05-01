83ad1a2106c046eac435b27a98ae5d0efdfdab05
Eve Zeyl Fiskebeck <evezeyl@gmail.com>
2025-05-02 00:19:01 +0200

    update protocole merging git

--- bin/utilities/merging_git_branches.sh
+++ bin/utilities/merging_git_branches.sh
@@ -1,3 +1,10 @@
 # !/usr/bin/env bash
 # script to faciliate updating and merging branches in git
 # as I often forget the commands
+
+# need to stage any other changes before merging
+git checkout main
+git pull origin main
+git checkout dev
+# git pull 
+
